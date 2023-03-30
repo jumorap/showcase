c69f2567f2d8ba851b7393f567552f65b1459496
ycuervob <ycuervob@users.noreply.github.com>
2023-03-30 06:01:49 +0000

    deploy: e2b1f1c306892b65531caeb260a3af63ac93e6eb

--- sketches/dither2.js
+++ sketches/dither2.js
@@ -2,6 +2,7 @@ let controlFactor = 0.1;
 let canvassizex = 680;
 let canvassizey = 400;
 let venus;
+let tileCount = 1;
 
 function preload() {
     venus = loadImage("/showcase/sketches/venus.jpg");
@@ -9,8 +10,12 @@ function preload() {
 
 function setup() {
     createCanvas(canvassizex, canvassizey);
-    image(venus, 0, 0);
     onleftpressed();
+    const slider = createSlider(1, 20, tileCount);
+    slider.position(0, 0);
+    slider.input(() => {
+        tileCount = slider.value();
+    });
 }
 
 function mouseClicked() {
@@ -20,9 +25,20 @@ function mouseClicked() {
     });
 }
 
+function draw() {
+    loadImage("/showcase/sketches/venus.jpg", function (img) {
+        venus = img;
+        onleftpressed();
+        fill(255);
+        rect(150, 0, 100, 20);
+        fill(0);
+        text("Using:" + pow(tileCount+1,3)+" colors!", 150, 18);
+    });
+}
+
 
 function myx(u, value) {
-    let steps = 5;
+    let steps = tileCount;
     return round(steps * round(u * value) / 255) * floor(255 / steps);
 }
 
